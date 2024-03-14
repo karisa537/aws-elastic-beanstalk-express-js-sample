2ffbdb4516d0f0c73a091d9d8086f07acd5b5180
Karisa Kalume <nyundobaya1@gmail.com>
2024-03-14 11:45:54 +0300

    finale

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Awesome!!'));
+app.get('/', (req, res) => res.send('hello!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
