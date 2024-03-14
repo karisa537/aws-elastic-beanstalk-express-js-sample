60133616f51845f7b2f08946a3e5eb6f097f62dd
Karisa Kalume <nyundobaya1@gmail.com>
2024-03-14 11:42:33 +0300

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Here you are!'));
+app.get('/', (req, res) => res.send('Awesome!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
